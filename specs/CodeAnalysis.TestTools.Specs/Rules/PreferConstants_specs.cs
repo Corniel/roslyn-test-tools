8e14c37e174ae472d1eab462b416fe472efe014c
Corniel Nobel <mail@corniel.nl>
2023-06-10 23:49:57 +0200

    Add a actual analyzer with a spec that tests its - limited - behavior.

---
+++ specs/CodeAnalysis.TestTools.Specs/Rules/PreferConstants_specs.cs
@@ -0,0 +1,11 @@
+﻿namespace Specs.Rules.PreferConstants_specs;
+
+public class Verifies
+{
+    [Test]
+    public void CSharp()
+        => new PreferConstants()
+        .ForCS()
+        .AddSource(@"Sources\PreferConstants.cs")
+        .Verify();
+}
---
+++ specs/CodeAnalysis.TestTools.Specs/Sources/PreferConstants.cs
@@ -0,0 +1,19 @@
+﻿internal static class Noncompliant
+{
+    public static int Unchanged()
+    {
+        var variable = 42; // Noncompliant {{'variable' can be a constant.}}
+//      ^^^^^^^^^^^^^^^^^^
+        return variable;
+    }
+}
+
+internal static class Compliant
+{
+    public static int Changed()
+    {
+        var variable = 42;
+        variable *= 3;
+        return variable;
+    }
+}
