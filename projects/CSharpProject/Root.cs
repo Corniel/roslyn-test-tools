2b27713a7b3782f1f742bc63707711228e7b34cb
Corniel Nobel <corniel@gmail.com>
2023-07-04 18:37:54 +0200

    Try CopyLocalLockFileAssemblies: true

--- projects/CSharpProject/Root.cs
+++ projects/CSharpProject/Root.cs
@@ -3,4 +3,6 @@
 public sealed class Root
 {
     public Qowaiv.Uuid Id { get; init; }
+
+    public static int Answer() => new MathNet.Numerics.Random.MersenneTwister().Next();
 }
--- projects/VbProject/Root.vb
+++ projects/VbProject/Root.vb
@@ -2,4 +2,8 @@ Public Class Root
 
     Public Property Id As Qowaiv.Uuid
 
+    Public Shared Function Answer() As Integer
+        Return New MathNet.Numerics.Random.MersenneTwister().Next()
+    End Function
+
 End Class
