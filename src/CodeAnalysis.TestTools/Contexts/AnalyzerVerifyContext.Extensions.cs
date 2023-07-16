fb7fca683d69931ac5bcd22673d77c0d45efb029
Corniel Nobel <corniel@gmail.com>
2023-07-16 14:47:20 +0200

    Add extra analyzers as extensions.

---
+++ src/CodeAnalysis.TestTools/Contexts/AnalyzerVerifyContext.Extensions.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeAnalysis.TestTools.Contexts;
+
+/// <summary>Extensions on <see cref="AnalyzerVerifyContext"/>.</summary>
+public static class AnalyzerVerifyContextExtensions
+{
+    /// <summary>Adds an (optional) extra analyzer.</summary>
+    [Pure]
+    public static TContext Add<TContext>(this TContext context, DiagnosticAnalyzer analyzer)
+        where TContext : AnalyzerVerifyContext
+        => context with
+        {
+            Analyzers = context.Analyzers.Add(analyzer),
+        };
+
+    /// <summary>Adds (optional) extra analyzers.</summary>
+    [Pure]
+    public static TContext Add<TContext>(this TContext context, params DiagnosticAnalyzer[] analyzers)
+        where TContext : AnalyzerVerifyContext
+        => context with
+        {
+            Analyzers = context.Analyzers.AddRange(analyzers),
+        };
+}
