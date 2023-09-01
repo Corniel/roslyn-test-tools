87635cd8f3d92c7f283833563ed3065fa2b9b51e
Corniel Nobel <corniel@gmail.com>
2023-09-01 20:14:31 +0200

    Use Buildanalyzer to build projects (#14)

--- src/CodeAnalysis.TestTools/ProjectLoader.cs
+++ src/CodeAnalysis.TestTools/ProjectLoader.cs
@@ -1,45 +1,17 @@
-﻿using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis.MSBuild;
+﻿using Buildalyzer;
+using Buildalyzer.Workspaces;
 
 namespace CodeAnalysis.TestTools;
 
-/// <summary>Can load <see cref="Project"/>s using a <see cref="MSBuildWorkspace"/>.</summary>
+/// <summary>Can load <see cref="Project"/>s using Buildalyzer.</summary>
 public static class ProjectLoader
 {
-    private static readonly object locker = new();
-
     /// <summary>Loads the project.</summary>
-    /// <remarks>
-    /// Registers the <see cref="MSBuildLocator.RegisterDefaults()"/>
-    /// if no MS build locater has been registered yet.
-    /// </remarks>
     public static Project Load(FileInfo location)
     {
-        Guard.Exists(location);
-        RegisterDefaults();
-        return Run.Sync(() => LoadAsync(location));
-    }
-
-    private static Task<Project> LoadAsync(FileInfo location)
-    {
-        using var workspace = MSBuildWorkspace.Create();
-        workspace.WorkspaceFailed += OnWorkspaceFailed;
-        return workspace.OpenProjectAsync(location.FullName);
-    }
-
-    private static void OnWorkspaceFailed(object? sender, WorkspaceDiagnosticEventArgs args)
-        => throw new InvalidOperationException(args.Diagnostic.Message);
-
-    private static void RegisterDefaults()
-    {
-        if (MSBuildLocator.IsRegistered) { return; }
-
-        lock (locker)
-        {
-            if (!MSBuildLocator.IsRegistered)
-            {
-                MSBuildLocator.RegisterDefaults();
-            }
-        }
+        var manager = new AnalyzerManager();
+        var analyzer = manager.GetProject(Guard.Exists(location).FullName);
+        var workspace = analyzer.GetWorkspace();
+        return workspace.CurrentSolution.Projects.Single();
     }
 }
