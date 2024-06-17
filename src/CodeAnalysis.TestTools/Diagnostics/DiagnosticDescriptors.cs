81699d6270aa866e048a735f16f6a9901f9929d6
Corniel Nobel <corniel@gmail.com>
2024-06-17 16:10:34 +0200

    Add equality comparer that compares DiagnosticDescriptor's on Id.

---
+++ src/CodeAnalysis.TestTools/Diagnostics/DiagnosticDescriptors.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeAnalysis.TestTools.Diagnostics;
+
+/// <summary><see cref="DiagnosticDescriptor"/> tooling.</summary>
+public static class DiagnosticDescriptors
+{
+    /// <summary>
+    /// Equality comparer for <see cref="DiagnosticDescriptor"/> based on
+    /// <see cref="DiagnosticDescriptor.Id"/>.
+    /// </summary>
+    public static readonly IEqualityComparer<DiagnosticDescriptor> ById = new EqualById();
+
+    private sealed class EqualById : IEqualityComparer<DiagnosticDescriptor>
+    {
+        /// <inheritdoc />
+        [Pure]
+        public bool Equals(DiagnosticDescriptor? x, DiagnosticDescriptor? y) => Equals(x?.Id, y?.Id);
+
+        /// <inheritdoc />
+        [Pure]
+        public int GetHashCode([DisallowNull] DiagnosticDescriptor obj) => obj.Id.GetHashCode();
+    }
+}
