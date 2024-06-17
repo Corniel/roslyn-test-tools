18f308e545addeaace319bfe7bbd0140fe87c1f1
Corniel Nobel <corniel@gmail.com>
2024-06-17 17:43:53 +0200

    With default.

--- src/CodeAnalysis.TestTools/References/NuGetPackage.Version.cs
+++ src/CodeAnalysis.TestTools/References/NuGetPackage.Version.cs
@@ -6,7 +6,7 @@ public partial class NuGetPackage
 {
     /// <summary>Gets the version of the package.</summary>
     [Pure]
-    public static async Task<NuGetVersion> GetVersionAsync(string packageId, string? version)
+    public static async Task<NuGetVersion> GetVersionAsync(string packageId, string? version = Latest)
         => string.IsNullOrEmpty(version) || version == Latest
             ? await NuGetRepository.GetLatestVersionAsync(packageId)
             : new NuGetVersion(version);
