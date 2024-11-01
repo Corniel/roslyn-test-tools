536f842212ec453e53a6790894a3893e2cce9a88
Corniel Nobel <corniel@gmail.com>
2024-11-01 12:42:28 +0100

    Documentation.

--- src/CodeAnalysis.TestTools/References/NuGetLatestVersionCheck.cs
+++ src/CodeAnalysis.TestTools/References/NuGetLatestVersionCheck.cs
@@ -3,7 +3,9 @@ namespace CodeAnalysis.TestTools.References;
 /// <summary>The latest version of a NuGet package and the time it was checked.</summary>
 public sealed record NuGetLatestVersionCheck()
 {
+    /// <summary>The version of the NuGet package.</summary>
     public string? Version { get; init; }
 
+    /// <summary>The last time the version have been checked.</summary>
     public required DateTime Checked { get; init; }
 }
